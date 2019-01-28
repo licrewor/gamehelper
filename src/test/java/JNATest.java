585e947abfa2efa36bee670b558703d6cc03ff29
licrewor <36726683+licrewor@users.noreply.github.com>
2019-01-28 09:49:00 +0800

    Update JNATest.java

--- src/test/java/JNATest.java
+++ src/test/java/JNATest.java
@@ -2,13 +2,6 @@ import com.crewor.gamehelper.jna.User32Lib;
 import com.crewor.gamehelper.jna.Win32Contants;
 import com.sun.jna.platform.win32.WinUser;
 
-/**
- * @author:likun
- * @email:likun18@jd.com
- * @erp:likun101
- * @CreateTime:2019/1/25 14:20
- * @Project notes
- */
 public class JNATest {
 
     public static void main(String[] args) {
